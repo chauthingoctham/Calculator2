b0f1d6dbca3a1173a4cd52034e9163b4c2a7368a
chautngoctham <thamchau1412@gmail.com>
2023-11-02 20:28:39 +0700

    Them dieu kien button Cong

--- Calculator2/Form1.cs
+++ Calculator2/Form1.cs
@@ -20,10 +20,15 @@ namespace Calculator2
         private void btCong_Click(object sender, EventArgs e)
         {
             int a, b, ketqua;
-            a = int.Parse(txta.Text);
-            b = int.Parse(txtb.Text);
-            ketqua = a + b;
-            txtKetqua.Text = ketqua.ToString();
+            if(txta.Text != String.Empty && txtb.Text !=String.Empty)
+            {
+                a = int.Parse(txta.Text);
+                b = int.Parse(txtb.Text);
+                ketqua = a + b;
+                txtKetqua.Text = ketqua.ToString();
+
+            }
+            
         }
 
         private void btTru_Click(object sender, EventArgs e)
